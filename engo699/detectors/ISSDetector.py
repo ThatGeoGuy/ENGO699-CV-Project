Fixed error in weighting / covariance calculation, made some changes so that a minimum number of nearest neighbours is required for a point to be used.

engo699/detectors/ISSDetector.py
@@ -6,6 +6,7 @@
 Defines the point detector used by the Intrinsic Shape Signatures algorithm.
 """
 
+import numpy as np
 from scipy import spatial
 
 class ISSKeypointDetector(object):
@@ -14,10 +15,13 @@ class ISSKeypointDetector(object):
     Signature algorithm for detecting keypoints.
     """
 
-    def __init__(self, points, density_radius, frame_radius):
-        self.kdtree = spatial.KDTree(points)
-        self.r_d = density_radius
-        self.r_f = frame_radius
+    def __init__(self, points, min_nn, density_radius, frame_radius, gamma21, gamma32):
+        self.kdtree         = spatial.KDTree(points)
+        self.min_neighbours = min_nn
+        self.r_d            = density_radius
+        self.r_f            = frame_radius
+        self.gamma21        = gamma21
+        self.gamma32        = gamma32
 
     @property
     def pts(self):
@@ -41,21 +45,84 @@ class ISSKeypointDetector(object):
         """
         w = []
 
-        for pt in self.pts:
-            dists, nn_indices = self.kdtree.query(pt, k=len(self.pts),
+        for i, pt_i in enumerate(self.pts):
+            dists, nn_indices = self.kdtree.query(pt_i, k=len(self.pts),
                     p=2, distance_upper_bound=self.r_d)
 
-            dists[dists == 0] = np.nan
-            w.append(np.nansum(1 / dists))
-        self.w = np.array(w)
-        return
+            # Remove NN indices where dists is infinite -> keep finites
+            nn_indices = nn_indices[np.isfinite(dists)]
+            # Remove the point itself from it's nearest neighbours
+            nn_indices = nn_indices[nn_indices != i]
 
-    def computeWeightedScatterMatrix(self):
+            tmp = np.nansum(np.linalg.norm(self.pts[nn_indices, :], 2, axis=1))
+            if tmp == 0:
+                w.append(1)
+            else:
+                w.append(1 / tmp)
+
+        return np.array(w)
+
+    def computeWeightedScatterMatrices(self, w):
         """
         Computes the weighted scatter matrix cov(pt_i) for pt_i using all points pt_j
         within a distance frame_radius.
         """
-        
-        for pt in self.pts:
-             
-        return
+        covars = []
+
+        for i, pt_i in enumerate(self.pts):
+            dists, nn_indices = self.kdtree.query(pt_i, k=len(self.pts),
+                    p=2, distance_upper_bound=self.r_f)
+
+            # Remove NN indices where dists is infinite -> keep finites
+            nn_indices = nn_indices[np.isfinite(dists)]
+            # Remove the point itself from it's nearest neighbours
+            nn_indices = nn_indices[nn_indices != i]
+
+            if len(nn_indices) < self.min_neighbours:
+                covars.append(np.zeros((3, 3)))
+                continue
+
+            tmp = np.zeros((3, 3))
+            for j, pt_j in enumerate(self.pts[nn_indices]):
+                # Matrix is necessary since the structure is needed for 1D array
+                diff = np.matrix(pt_j - pt_i)
+                tmp += w[nn_indices[j]] * (diff.T * diff)
+            covars.append(tmp / np.sum(w[nn_indices]))
+        return covars
+
+    def detectKeypoints(self):
+        """
+        Detects and returns the indices of points determined to be keypoints via the
+        ISS saliency measure with non-maxima suppression.
+        """
+        w = self.computeWeights()
+        covars = self.computeWeightedScatterMatrices(w)
+
+        saliencies = np.zeros(len(self.pts))
+        for i, CV in enumerate(covars):
+            eig_vals, eig_vecs = np.linalg.eigh(CV)
+
+            if not np.all(eig_vals == 0):
+                lam1 = np.max(eig_vals)
+                lam2 = np.max(eig_vals[np.array([0, 1, 2]) != np.argmax(eig_vals)])
+                lam3 = np.min(eig_vals)
+
+                if lam3 < 0:
+                    continue
+
+                if (lam2 / lam1) < self.gamma21 and (lam3 / lam2) < self.gamma32:
+                    saliencies[i] = lam3
+
+        self.keypoint_indices = []
+        for i, pt_i in enumerate(self.pts):
+            dists, nn_indices = self.kdtree.query(pt_i, k=len(self.pts),
+                    p=2, distance_upper_bound=self.r_f)
+
+            # Remove NN indices where dists is infinite -> keep finites
+            nn_indices = nn_indices[np.isfinite(dists)]
+            # Remove the point itself from it's nearest neighbours
+            nn_indices = nn_indices[nn_indices != i]
+
+            if nn_indices.tolist() and saliencies[i] >= np.max(saliencies[nn_indices]):
+                self.keypoint_indices.append(i)
+        return self.pts[self.keypoint_indices, :].copy()

tests/test_engo699_detectors_ISSDetector.py
@@ -21,14 +21,20 @@ class TestISSKeypointDetector(unittest.TestCase):
         Tests that the object initializes properly.
         """
         points         = np.ones((5000, 3))
+        min_nn         = 8
         density_radius = 0.040
         frame_radius   = 1.900
+        gamma21        = 1.00
+        gamma32        = 1.00
 
-        iss = ISSKeypointDetector(points, density_radius, frame_radius)
+        iss = ISSKeypointDetector(points, min_nn, density_radius, frame_radius, gamma21, gamma32)
 
         self.assertTrue(np.all(points == iss.pts))
         self.assertEqual(density_radius, iss.r_d)
         self.assertEqual(frame_radius, iss.r_f)
+        self.assertEqual(min_nn, iss.min_neighbours)
+        self.assertEqual(gamma21, iss.gamma21)
+        self.assertEqual(gamma32, iss.gamma32)
 
     def testComputeWeights(self):
         """